a896fec54c4a166ed22cc07a0febf5746c402106
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-02-21 11:55:48 -0500

    Add solution

---
+++ com/thinkful/app/SimpleStringFormatting.java
@@ -0,0 +1,11 @@
+package com.thinkful.app;
+
+public class SimpleStringFormatting {
+    public static void main(String[] args) {
+        System.out.println("1. Avengers: Endgame\n\tRobert Downey Jr., Scarlett Johansson");
+        System.out.println("2. Captain Marvel \n\tBrie Larson, Samuel L. Jackson ");
+        System.out.println("3. Long Shot\n\tCharlize Theron, Seth Rogen");
+        System.out.println("4. Hustlers\n\tConstance Wu, Jennifer Lopez");
+        System.out.println("5. Ad Astra\n\tBrad Pitt, Tommy Lee Jones ");
+    }
+}
